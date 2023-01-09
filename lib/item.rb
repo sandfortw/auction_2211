36b785ee120cf8fc77703cfd5095c90fb3fa2e7f
Weston Sandfort <sandfortw@gmail.com>
2023-01-08 19:20:48 -0700

    Add item class and update spec file

---
+++ lib/item.rb
@@ -0,0 +1,10 @@
+class Item
+  attr_reader :name
+
+  def initialize(name)
+    @bids = {}
+    @name = name
+  end
+    
+
+end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -3,11 +3,15 @@ require './lib/item'
 describe Item do
   let(:item1) {Item.new('Chalkware Piggy Bank')}
   describe 'initialization' do
-    expect(item1)to be_instance_of(Item)
+    it 'exists' do
+    expect(item1).to be_instance_of(Item)
+    end
   end
 
   describe '#name' do
-    expect(item1.name).to eq("Chalkware Piggy Bank")
+    it 'has a name' do
+      expect(item1.name).to eq("Chalkware Piggy Bank")
+    end
   end
 
 end
