e6a4242d6311f3bcff09a5aa625a3a4af718433c
Weston Sandfort <sandfortw@gmail.com>
2023-01-08 19:31:38 -0700

    Add attendee class"

--- spec/attendee_spec.rb
+++ spec/attendee_spec.rb
@@ -11,13 +11,13 @@ describe Attendee do
   end
 
   describe '#name' do
-    it 'returns the name'
+    it 'returns the name' do
       expect(attendee.name).to eq 'Megan'
     end
   end
 
   describe '#budget' do
-    it 'returns the budget'
+    it 'returns the budget' do
       expect(attendee.budget).to eq '50'
     end
   end
